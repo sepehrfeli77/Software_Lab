3ae6d516fbbfe4b35c6dd07ac0ab52140cd68d53
sina <sinakazemi1998@gmail.com>
2021-11-02 18:15:41 +0330

    Part1(Rectangle) Tests Handled!

--- Exp 3/src/main/java/Rectangle.java
+++ Exp 3/src/main/java/Rectangle.java
@@ -3,6 +3,10 @@ public class Rectangle implements CalculateArea {
     private int height;
 
     public Rectangle(int width, int height) throws IllegalArgumentException {
+        if (height <= 0 || width <= 0) {
+            throw new IllegalArgumentException("Width or Height must be positive!");
+        }
+
         this.width = width;
         this.height = height;
     }
@@ -12,4 +16,6 @@ public class Rectangle implements CalculateArea {
     public int computeArea() {
         return width * height;
     }
+
+
 }
