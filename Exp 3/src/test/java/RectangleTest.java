578818e6713f71147d245ac58537170d4e253020
sina <sinakazemi1998@gmail.com>
2021-11-02 17:47:53 +0330

    Test Was Written!

---
+++ Exp 3/src/test/java/RectangleTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RectangleTest {
+    @Test
+    public void calculateAreaOnPositiveWidthAndHeight(){
+        int width = 2;
+        int height = 5;
+        Rectangle rectangle = new Rectangle(width, height);
+
+        int expectedArea = width * height;
+        int calculatedArea = rectangle.calculateArea();
+
+        assertEquals(expectedArea, calculatedArea);
+    }
+    @Test
+    public void calculateAreaOnNotPositiveWidthAndHeight(){
+        int width = -3;
+        int height = 2;
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Rectangle(width, height);
+        });
+
+        String expectedMessage = "Width or Height must be positive!";
+        String gotMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, gotMessage);
+    }
+
+}
