2902d5fab22d42ea8f3d96ef39f88fdc329d0a61
Sepehr Feli <sepehr.feli.1377@gmail.com>
2021-11-09 00:29:16 +0330

    initial commit. Abstract Factorry project created and tests added

---
+++ Exp 4/src/test/java/AbstractFactory/GardenCreatorTest.java
@@ -0,0 +1,70 @@
+package AbstractFactory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GardenCreatorTest {
+    @Test
+    public void IranianGardenWithIranianTreesAndFlowers(){
+        IranianGardenBuilder iranianGardenBuilder = new IranianGardenBuilder();
+        String expectedFlowerMessage = "Iranian flower created";
+        String expectedTreeMessage = "Iranian Tree created";
+
+        IranianFlower iranianFlower = (IranianFlower) iranianGardenBuilder.createFlower();
+        String realFlowerMessage = iranianFlower.getMessage();
+
+        IranianTree iranianTree = (IranianTree) iranianGardenBuilder.createTree();
+        String realTreeMessage = iranianTree.getMessage();
+
+        assertEquals(realFlowerMessage, expectedFlowerMessage);
+        assertEquals(realTreeMessage, expectedTreeMessage);
+    }
+
+    @Test
+    public void IranianGardenWithJapaneseTrees(){
+        IranianGardenBuilder iranianGardenBuilder = new IranianGardenBuilder();
+        assertThrows(ClassCastException.class, () -> {
+            JapaneseTree japaneseTree = (JapaneseTree) iranianGardenBuilder.createTree();
+        });
+    }
+
+    @Test
+    public void IranianGardenWithJapaneseFlowers(){
+        IranianGardenBuilder iranianGardenBuilder = new IranianGardenBuilder();
+        assertThrows(ClassCastException.class, () -> {
+            JapaneseFlower japaneseFlower = (JapaneseFlower) iranianGardenBuilder.createFlower();
+        });
+    }
+
+    public void JapaneseGardenWithJapaneseTreesAndFlowers(){
+        JapaneseGardenBuilder japaneseGardenBuilder = new JapaneseGardenBuilder();
+        String expectedFlowerMessage = "Japanese flower created";
+        String expectedTreeMessage = "Japanese Tree created";
+
+        JapaneseFlower japaneseFlower = (JapaneseFlower) japaneseGardenBuilder.createFlower();
+        String realFlowerMessage = japaneseFlower.getMessage();
+
+        JapaneseTree japaneseTree = (JapaneseTree) japaneseGardenBuilder.createTree();
+        String realTreeMessage = japaneseTree.getMessage();
+
+        assertEquals(realFlowerMessage, expectedFlowerMessage);
+        assertEquals(realTreeMessage, expectedTreeMessage);
+    }
+
+    @Test
+    public void JapaneseGardenWithIranianTrees(){
+        JapaneseGardenBuilder japaneseGardenBuilder = new JapaneseGardenBuilder();
+        assertThrows(ClassCastException.class, () -> {
+            IranianTree iranianTree = (IranianTree) japaneseGardenBuilder.createTree();
+        });
+    }
+
+    @Test
+    public void JapaneseGardenWithIranianFlowers(){
+        JapaneseGardenBuilder japaneseGardenBuilder = new JapaneseGardenBuilder();
+        assertThrows(ClassCastException.class, () -> {
+            IranianFlower iranianFlower = (IranianFlower) japaneseGardenBuilder.createFlower();
+        });
+    }
+}
