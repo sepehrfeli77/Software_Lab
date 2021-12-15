a9135122f0f4556b8a52cdc81d2aa3d058ec084b
sina <sinakazemi1998@gmail.com>
2021-12-15 11:56:43 +0330

    add prescription to the models

--- Exp 7,8/user_account/models.py
+++ Exp 7,8/user_account/models.py
@@ -19,6 +19,7 @@ class Doctor(db.Model):
     national_id =  db.Column(db.String(10), unique = True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    prescriptions = db.relationship('Prescription', backref='doctor')
 
     def to_dict(self):
         vals = vars(self)
@@ -33,6 +34,7 @@ class Patient(db.Model):
     national_id =  db.Column(db.String(10), unique = True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    prescriptions = db.relationship('Prescription', backref='patient')
 
     def to_dict(self):
         vals = vars(self)
@@ -40,3 +42,16 @@ class Patient(db.Model):
 
     def __repr__(self):
         return self.name
+
+class Prescription(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    doctor_id =  db.Column(db.Integer, db.ForeignKey('doctor.id'))
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'))
+    comment = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        vals = vars(self)
+        return {attr: vals[attr] for attr in vals if 'instance_state' not in attr}
+
+    def __repr__(self):
+        return f"{self.doctor_id} -> {self.patient_id}"
